019645707474be59ee4c5e96b6b082f426e0e0ca
takepo <takeshima.nahoko@lancers.co.jp>
2017-10-09 15:58:44 +0900

    feat: add 2

---
+++ 2/2-7-6.py
@@ -0,0 +1,11 @@
+name = ''
+while True:
+    print('あなたはだれ？')
+    name = input()
+    if name != 'Joe':
+        continue
+    print('こんにちはJoe。パスワードは何？(魚の名前)')
+    password = input()
+    if password == 'swordfish':
+        break
+print('認証しました')

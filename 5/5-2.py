34c7de105183c83f5790fce532938327aedbdcae
takepo <takeshima.nahoko@lancers.co.jp>
2017-10-16 00:47:03 +0900

    feat add 5-2 sample code

---
+++ 5/5-2.py
@@ -0,0 +1,9 @@
+import pprint
+message = 'It was a bright cold say in Aplil, and the clocks were striking thirteen.'
+count = {}
+
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+pprint.pprint(count)
